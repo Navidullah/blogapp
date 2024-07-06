2ef2bc6f38018aa7f296606753451aa9d1d54ddb
komal ,k c~x <komaalfarid@gmail.com>
2024-07-06 10:44:09 +0500

    initial commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute/user.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("app is running on 3000 port");
 });
+app.use("/api/user", userRoutes);
--- client/src/components/AuthLinks.jsx
+++ client/src/components/AuthLinks.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 const AuthLinks = () => {
-  const status = "authenticated";
+  const status = "unauthenticated";
   return (
     <div className="setcol text-[18px] font-normal text-center py-2  hover:bg-lightcriBlue w-full max-[1024px]:border-b">
       {status === "unauthenticated" ? (
